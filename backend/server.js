4f9d40b56f8558bc5bac06da481f8183f9845823
Braxton Toomer <braxton.toomer@gmail.com>
2019-12-04 23:22:34 -0800

    Adds models directory to backend and configures user model

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
