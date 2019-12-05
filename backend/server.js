fbdd558f2c536559061a26b23d6941ca3ef69339
Braxton Toomer <braxton.toomer@gmail.com>
2019-12-04 23:34:28 -0800

    adds route directories and requires in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
